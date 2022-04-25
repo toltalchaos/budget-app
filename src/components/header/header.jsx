867f0cbb4f7bcb1503bf7dfc998b9bb6eb895e92
Braydon Tol <braydon.tol@drivewyze.com>
2022-04-25 11:51:30 -0600

    add styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import '../../assets/hamburgers.css'
 import BurgerButton from '../bits/burger-button';
 import styled from 'styled-components'
+import  {MAIN_COLOR, MAIN_COLOR_LIGHT, SECOND_COLOR, SECOND_COLOR_LIGHT, THIRD_COLOR} from '../../assets/const-values';
 
 const HEADERSECTION = styled.section`
+color: ${SECOND_COLOR};
+background-color: ${MAIN_COLOR};
+border-radius: 0 0 1rem 1rem;
 h1{
     margin: 0;
 }
 div{
     padding: 0.5rem;
     margin: auto;
-    background-color: tomato;
+    background-color: ${MAIN_COLOR};
     display: flex;
     justify-content: end;
 
@@ -23,10 +27,10 @@ position: relative;
 text-align: center;
 .navdeck{
     font-size: 2rem;
-    background-color: aqua;
+    background-color: ${MAIN_COLOR};
     right: 0;
     left: auto;
-    border-radius: 2rem;
+    border-radius: 1rem 0 1rem 1rem;
     button{size:1rem;}
     ul{
         display: flex;
@@ -38,7 +42,7 @@ text-align: center;
         list-style: none;
         padding: 0.25rem 0.5rem;
         margin: 0.05rem 0.5rem;
-        background-color: beige;
+        background-color: ${MAIN_COLOR_LIGHT};
         border-radius: 1rem ;
         vertical-align: middle;
         }
@@ -46,6 +50,14 @@ text-align: center;
 }
 .menu-incomes:hover{
     cursor: pointer;
+}
+.incomes{
+    padding: 0.5rem;
+}
+
+.income-input{
+    width: 5rem;
+    border-radius: 0.5rem;
 }
 
 `
@@ -77,9 +89,9 @@ const HeaderSection = (props) => {
                 </li>
                 <li> <a href={`/`}><p>home</p></a></li>
                 <li >
-                    <p className='menu-incomes' onClick={handleIncomesClick}>test incomes <BurgerButton loc='income'/> <br/><br/></p>
+                    <p className='menu-incomes' onClick={handleIncomesClick}>Incomes <BurgerButton loc='income'/> <br/><br/></p>
                     <ul className='incomes navdeck hide-me'>
-                        <li>1,2,3</li>
+                        <input type="number" className='income-input' min={1} max={9}/>
                     </ul>
                 </li>
             </ul>
